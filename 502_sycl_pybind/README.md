7f2286b3b16fb75bbe7ccbf9e66591f851a5bbfc
Ben <benalmer1983@gmail.com>
2025-11-03 04:58:41 +0000

    More notes added

---
+++ 502_sycl_pybind/README.md
@@ -0,0 +1,2 @@
+ apt update
+apt install python3.10-venv python3.10-dev -y
--- 502_sycl_pybind/run_gpu.sh
+++ 502_sycl_pybind/run_gpu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /opt/intel/oneapi/setvars.sh
+source .venv/bin/activate
 export SYCL_DEVICE_FILTER=cuda
 
 python3 use_maths.py
